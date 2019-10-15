6439d31fc200153b7586e6af7624a2d8e24dad76
AsTek007 <AsTekGitHub@gmail.com>
2019-10-15 19:09:39 +0200

    MrMme V0.0.01 : find vane from DB V.1  	-Set logs in English

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var global = require('./global.js');
 var utils = require('./utils.js');
 
-utils.logH1(" LANCEMENT DE L'APPLICATION");
+utils.logH1(" Application Start");
 
 // Modules
 var express = require('express');
@@ -15,27 +15,23 @@ var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 
 
 mongoose.connect(global.mongoDBurl, options);
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
-db.once('open', function (){  console.log("Connexion à la base OK");});
+db.on('error', console.error.bind(console, 'Error when trying to connect to the DataBase'));
+db.once('open', function (){  console.log("Connected to the DataBase");});
 
 require('./models/Vane');
 require('./models/Prenom');
 
-
 var app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/prenoms', require('./routes/prenoms'))
 app.use('/', require('./routes/vanes'))
-
 
 nunjucks.configure('views', {
 	autoescape: true,
 	express: app
 });
 
-console.log(global.appName + ' lancée sur le port ' + global.port);
+console.log(global.appName + ' started on the port ' + global.port);
 app.listen(global.port);
